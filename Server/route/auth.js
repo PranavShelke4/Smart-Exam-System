9afc43618a0c354a8b8740092a2666c87447ddf4
Pranav <pranavshelke4@gmail.com>
2023-08-18 19:58:16 +0530

    All Student

--- Server/route/auth.js
+++ Server/route/auth.js
@@ -161,6 +161,33 @@ router.get("/admin-dashbord", AuthenticateAdmin, (req, res) => {
   res.send(req.rootAdmin);
 });
 
+// Get All Student
+router.get("/all-students", AuthenticateAdmin, async (req, res) => {
+  try {
+    const students = await User.find();
+    res.status(200).json({ students });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//Delete The Student
+router.delete("/delete-student/:id", AuthenticateAdmin, async (req, res) => {
+  try {
+    const student = await User.findById(req.params.id);
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    await student.remove();
+    res.status(200).json({ message: "Student deleted" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 // Get All Admin
 router.get("/all-admins", AuthenticateAdmin, async (req, res) => {
   try {
--- client/src/component/admin/AllStudent.js
+++ client/src/component/admin/AllStudent.js
@@ -1,47 +1,101 @@
-import React, {useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
+import Table from "react-bootstrap/Table";
 
 function AllStudent() {
-    const navigate = useNavigate();
-    const [userData, setUserData] = useState({});
+  const [students, setStudents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const callAdminDashbordPage = async () => {
-        try {
-          const res = await fetch("/admin-dashbord", {
-            method: "GET",
-            headers: {
-              Accept: "application/json",
-              "Content-type": "application/json",
-            },
-            credentials: "include",
-          });
-    
-          console.log("Response status:", res.status);
-    
-          if (res.status === 200) {
-            const data = await res.json();
-            setUserData(data);
-          } else if (res.status === 401) {
-            navigate("/admin-login");
-          } else {
-            throw new Error("Failed to fetch admin dashboard");
-          }
-        } catch (err) {
-          console.log("Error:", err);
-        }
-      };
-    
-      useEffect(() => {
-        callAdminDashbordPage();
-      }, []);
-    
+  const fetchAllStudents = async () => {
+    try {
+      const res = await fetch("/all-students", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        setStudents(data.students);
+      } else {
+        throw new Error("Failed to fetch student data");
+      }
+    } catch (err) {
+      console.log("Error:", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllStudents();
+  }, []);
+
+  const handleDelete = async (studentId) => {
+    try {
+      const res = await fetch(`/delete-student/${studentId}`, {
+        method: "DELETE",
+        credentials: "include",
+      });
+
+      if (res.status === 200) {
+        // Refresh the student list
+        fetchAllStudents();
+        window.alert("Student Delete Successful");
+      } else {
+        throw new Error("Failed to delete student");
+      }
+    } catch (err) {
+      console.log("Error:", err);
+    }
+  };
+
   return (
     <div style={{ display: "flex", height: "100vh", minHeight: "400px" }}>
-        <Sidebar />
-        <main style={{ padding: 10 }}>hii</main>
+      <Sidebar />
+      <main style={{ padding: 10 }}>
+        <div className="container mt-4">
+          <h1>All Students</h1>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Sr No</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone Number</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {students.map((student, index) => (
+                  <tr key={student._id}>
+                    <td>{index + 1}</td>
+                    <td>{student.name}</td>
+                    <td>{student.email}</td>
+                    <td>{student.number}</td>
+                    <td>
+                      <button
+                        onClick={() => handleDelete(student._id)}
+                        className="btn btn-danger"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default AllStudent
+export default AllStudent;
