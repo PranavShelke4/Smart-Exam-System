0a2e1329f158edc389aa220df1b689580da9da02
Pranav <pranavshelke4@gmail.com>
2023-10-01 03:12:21 +0530

    full screen mode add

--- client/src/component/student/TestPage.js
+++ client/src/component/student/TestPage.js
@@ -9,6 +9,9 @@ function TestPage() {
   const [selectedAnswers, setSelectedAnswers] = useState({});
   const [totalMarks, setTotalMarks] = useState(0);
 
+  // Track the number of times the student exits fullscreen mode
+  const [exitFullscreenCount, setExitFullscreenCount] = useState(0);
+
   const callStudentDashboardPage = async () => {
     try {
       const res = await fetch("/student-dashbord", {
@@ -61,7 +64,56 @@ function TestPage() {
     }));
   };
 
- 
+  const handleLogout = async () => {
+    try {
+      const res = await fetch("/logout", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (res.status === 200) {
+        window.location.href = "/login";
+      } else {
+        const error = new Error("Failed to logout");
+        throw error;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleFullscreenChange = () => {
+    if (!document.fullscreenElement) {
+      // Student exited fullscreen mode
+      setExitFullscreenCount((prevCount) => prevCount + 1);
+
+      // Show a popup alert
+      window.confirm("You exited fullscreen mode. Click OK to re-enter fullscreen mode.");
+
+      if (exitFullscreenCount >= 0) {
+        handleLogout();
+      }
+    }
+  };
+
+  const enterFullscreen = () => {
+    const element = document.documentElement;
+
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+  };
+
   const handleSubmit = async () => {
     try {
       let marks = 0;
@@ -103,41 +155,55 @@ function TestPage() {
   useEffect(() => {
     fetchQuestions();
     callStudentDashboardPage();
+
+    // Add a fullscreen change event listener
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Automatically enter fullscreen mode when the component loads
+    enterFullscreen();
   }, []);
 
   return (
     <div className="container mt-4">
-    <h2 className="mb-4">Test Page</h2>
-    <h1>Student Test Page</h1>
-    <p>Test ID: {testId}</p>
-    {questions.map((question) => (
-      <div key={question._id} className="mb-4">
-        <h5>{question.question}</h5>
-        <ul>
-          {question.options.map((option) => (
-            <li key={option}>
-              <label>
-                <input
-                  type="radio"
-                  value={option}
-                  checked={selectedAnswers[question._id] === option}
-                  onChange={() => handleAnswerSelect(question._id, option)}
-                />
-                {option}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-    ))}
+      <h2 className="mb-4">Test Page</h2>
+      <h1>Student Test Page</h1>
+      <p>Test ID: {testId}</p>
 
-    <button className="btn btn-primary" onClick={handleSubmit}>
-      Submit Test
-    </button>
+      {questions.map((question) => (
+        <div key={question._id} className="mb-4">
+          <h5>{question.question}</h5>
+          <ul>
+            {question.options.map((option) => (
+              <li key={option}>
+                <label>
+                  <input
+                    type="radio"
+                    value={option}
+                    checked={selectedAnswers[question._id] === option}
+                    onChange={() => handleAnswerSelect(question._id, option)}
+                  />
+                  {option}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
 
-    <p>Total Marks: {totalMarks}</p>
-  </div>
-);
+      <button className="btn btn-primary" onClick={handleSubmit}>
+        Submit Test
+      </button>
+
+      <p>Total Marks: {totalMarks}</p>
+    </div>
+  );
 }
 
 export default TestPage;
