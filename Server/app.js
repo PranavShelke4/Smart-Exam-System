e2de4bcef07d2ab72b7248de49817c11afbd3a8d
Pranav <pranavshelke4@gmail.com>
2023-12-06 12:47:02 +0530

    .

--- Server/app.js
+++ Server/app.js
@@ -27,7 +27,7 @@ app.use(require('./route/admin/admins'));
 
 app.use(require('./route/student/student'));
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
